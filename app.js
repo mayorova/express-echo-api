80a60e759e2819345d2a33f9b6dc66385afa7557
Daria Mayorova <mayorova@users.noreply.github.com>
2022-12-22 12:01:53 +0100

    Refactoring

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
 const express = require('express');
-const winston = require('winston');
-const util = require('util');
 const cors = require('cors');
-
-const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    // winston.format.simple()
-    winston.format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
-  ),
-  transports: [
-    new winston.transports.Console()
-  ]
-});
+const logger = require('./lib/logger.js');
+const { reqData, inspect } = require('./lib/utils');
 
 const app = express();
 
@@ -23,25 +10,25 @@ const port = process.env.PORT || 3000,
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+// app.use(cors())
+
+// Log the request
+app.use(function (req, res, next) {
+  logger.info('-----------------------------------');
+  logger.info(inspect(reqData(req)));
+  logger.info('-----------------------------------\n\n');
+  next();
+});
+
+// "Catch-all" handler
+app.all('*', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  const response = reqData(req);
+  res.status(200).json(response);
+});
 
 app.listen(port, ip, function () {
   logger.info('Echo API is listening on port ' + port );
-});
-
-const reqData = function(req) {
-  const params = ['method', 'hostname', 'path', 'query', 'headers', 'body'];
-  return params.reduce(
-    (accumulator, currentValue) => { accumulator[currentValue] = req[currentValue]; return accumulator},
-    {}
-  );
-};
-
-app.all('*', function (req, res) {
-  logger.info(util.inspect(reqData(req), false, null, true));
-  res.set('Content-Type', 'application/json');
-  const response = reqData(req);
-  res.status(200).send(JSON.stringify(response,null,2));
 });
 
 module.exports = app;
